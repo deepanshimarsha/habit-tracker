977b6b0c1dd88a84d9f756c7b6ab16a0412b4dc5
Deepanshi Sharma <deepanshisharma2510@gmail.com>
2023-06-18 21:38:51 +0530

    habit detail refactor

--- src/component/HabitDetail/DetailModal.js
+++ src/component/HabitDetail/DetailModal.js
@@ -13,14 +13,21 @@ export default function DetailModal(habit) {
       </Button>
       <Modal show={show} onHide={handleClose}>
         <Modal.Header>
-          <Modal.Title>New Habit</Modal.Title>
+          <Modal.Title>{habit.name}</Modal.Title>
         </Modal.Header>
         <Modal.Body>
-          <h3>Habit : {habit.name}</h3>
-          <p>Goal : {habit.goal}</p>
-          <p>Repeat :{habit.repeat}</p>
-          <p>Time :{habit.time}</p>
-          <p>Date :{habit.date}</p>
+          <p>
+            <b>Goal</b> : {habit.goal}
+          </p>
+          <p>
+            <b>Repeat</b> : {habit.repeat}
+          </p>
+          <p>
+            <b>Time of day</b> : {habit.time}
+          </p>
+          <p>
+            <b>Start Date</b> : {habit.date}
+          </p>
         </Modal.Body>
         <Modal.Footer>
           <Button variant="danger" onClick={handleClose}>
